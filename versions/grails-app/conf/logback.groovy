0576dd4c315fda6d97edb4c1cc0be3a5c2f178ac
Eric Helgeson <erichelgeson@gmail.com>
2020-07-16 09:17:54 -0500

    3.2.8

--- versions/grails-app/conf/logback.groovy
+++ versions/grails-app/conf/logback.groovy
@@ -32,8 +32,5 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
         }
     }
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
-    root(ERROR, ['STDOUT', 'FULL_STACKTRACE'])
 }
-else {
-    root(ERROR, ['STDOUT'])
-}
+root(ERROR, ['STDOUT'])
