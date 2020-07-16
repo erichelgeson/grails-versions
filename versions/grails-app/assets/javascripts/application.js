3c03746a1f1fff4541fe3ac231cb7b1a7a4bcea8
Eric Helgeson <erichelgeson@gmail.com>
2020-07-16 09:22:07 -0500

    3.3.9

--- versions/grails-app/assets/javascripts/application.js
+++ versions/grails-app/assets/javascripts/application.js
@@ -5,17 +5,7 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //
-//= require jquery-2.2.0.min
+//= require jquery-3.3.1.min
 //= require bootstrap
-//= require_tree .
+//= require popper.min
 //= require_self
-
-if (typeof jQuery !== 'undefined') {
-    (function($) {
-        $(document).ajaxStart(function() {
-            $('#spinner').fadeIn();
-        }).ajaxStop(function() {
-            $('#spinner').fadeOut();
-        });
-    })(jQuery);
-}
