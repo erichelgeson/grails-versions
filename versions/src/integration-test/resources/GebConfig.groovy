60ac5f612793ac97f838ce7b08099b2096748b37
Eric Helgeson <erichelgeson@gmail.com>
2020-07-16 09:23:06 -0500

    4.0.0.M1

--- versions/src/integration-test/resources/GebConfig.groovy
+++ versions/src/integration-test/resources/GebConfig.groovy
@@ -1,15 +1,15 @@
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.firefox.FirefoxDriver
 
 environments {
 
-    // run via “./gradlew -Dgeb.env=chrome -Dwebdriver.chrome.driver=/Applications/chromedriver iT”
+    // run via “./gradlew -Dgeb.env=chrome iT”
     chrome {
         driver = { new ChromeDriver() }
     }
 
-    // run via “./gradlew -Dgeb.env=chromeHeadless -Dwebdriver.chrome.driver=/Applications/chromedriver iT”
+    // run via “./gradlew -Dgeb.env=chromeHeadless iT”
     chromeHeadless {
         driver = {
             ChromeOptions o = new ChromeOptions()
@@ -18,8 +18,8 @@ environments {
         }
     }
 
-    // run via “./gradlew -Dgeb.env=htmlUnit iT”
-    htmlUnit {
-        driver = { new HtmlUnitDriver() }
+    // run via “./gradlew -Dgeb.env=firefox iT”
+    firefox {
+        driver = { new FirefoxDriver() }
     }
 }
